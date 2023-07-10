c08736606f55765ceefb430270784cb140f4d128
julien himmer <julienhimmer@MacBook-Pro-de-julien.local>
2023-07-10 12:05:13 +0200

    homework done

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -4,14 +4,33 @@ import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable ;
+
+import org.springframework.http.*;
+import org.springframework.web.server.*;
+
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable int number) {
 
-        return null;
+    if(1 <= number && number <= 12)
+    {
+        throw new ResponseStatusException(HttpStatus.SEE_OTHER, "");
+
+    }
+    else if(number == 13)
+    {
+                return new Doctor(number, "Jodie Whittaker");
+    }
+
+    else
+    {
+
+    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " +Integer.toString(number));
+    }
     }
 }
